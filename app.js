e74d917a987b32018422832bb1f82dbf9d815d00
Kevin Schmidt <NSPtuning@gmail.com>
2017-02-14 17:47:28 -0800

    end of day commit

--- app.js
+++ app.js
@@ -2,26 +2,28 @@
 
 var openTimes = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
 
-function CookieStore(location, minCustomers, maxCustomers, avgCookies, cookiesPerHour) {
+function CookieStore(location, minCustomers, maxCustomers, avgCookies) {
   this.location = location;
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgCookies = avgCookies;
+  this.cookiesPerHourAr = [];
 
   this.custPerHour = function() {
     return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers));
 
-    this.cookiesPerHour = function() {
-      for (var i = 6; i < 20; i++){
-        return this.cookiesPerHour(Math.floor(this.custPerHour() * this.avgCookies));
-      }
+  };
+  this.cookiesPerHour = function() {
+    for (var i = 0; i < 15; i++){
+      var avgCookiesPerHour = Math.floor(this.custPerHour() * this.avgCookies);
+      this.cookiesPerHourAr.push(avgCookiesPerHour);
     };
   };
   // console.log(this.location);
   // console.log(this.minCustomers);
   // console.log(this.maxCustomers);
   // console.log(this.avgCookies);
-  console.log(this.cookiesPerHour);
+  // console.log(CookieStore.cookiesPerHour);
 }
 
 var storeOne = new CookieStore('1st and Pike', 23, 65, 6.3, []);
@@ -41,7 +43,11 @@ console.log(storeFive);
 
 var stores = [storeOne, storeTwo, storeThree, storeFour, storeFive];
 
-console.log(storeOne.location);
+for(var i = 0; i < stores.length; i++){
+  stores[0++]();
+}
+
+// console.log(storeOne.location);
 
 var tableEl = document.createElement('table');
 
@@ -53,6 +59,10 @@ for(var i = 0; i < stores.length; i++){
 
   var nameEl = document.createElement('th');
   nameEl.textContent = currentStore.name;
+
+  // for(var i = 0; i < stores.length; i++){
+  //   var totot;
+  // };
   // <th>currentStore</th>
   rowEl.appendChild(nameEl);
 
@@ -67,6 +77,37 @@ for(var i = 0; i < stores.length; i++){
   var aveCookiesEl = document.createElement('td');
   aveCookiesEl.textContent = currentStore.avgCookies;
   rowEl.appendChild(aveCookiesEl);
+
+};
+
+document.body.appendChild(tableEl);
+
+var tableEl = document.createElement('table');
+
+for(var i = 0; i < stores.length; i++){
+  var currentStore = stores[i];
+  var rowEl = document.createElement('tr');
+  tableEl.appendChild(rowEl);
+
+  var nameEl = document.createElement('th');
+  nameEl.textContent = currentStore.name;
+  rowEl.appendChild(nameEl);
+
+  var minCustEl = document.createElement('td');
+  minCustEl.textContent = currentStore.minCustomers;
+  rowEl.appendChild(minCustEl);
+
+  var maxCustEl = document.createElement('td');
+  maxCustEl.textContent = currentStore.maxCustomers;
+  rowEl.appendChild(maxCustEl);
+
+  var aveCookiesEl = document.createElement('td');
+  aveCookiesEl.textContent = currentStore.avgCookies;
+  rowEl.appendChild(aveCookiesEl);
+
+  var cookiesPerHourEl = document.createElement('td');
+  cookiesPerHourEl.textContent = currentStore.cookiesPerHourAr;
+  rowEl.appendChild(cookiesPerHourEl);
 
 };
 
